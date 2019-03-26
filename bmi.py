fb80f06b991d2e1ed46d892c6b755aba3288287c
G <36684964+grdguez@users.noreply.github.com>
2019-03-26 18:54:35 -0400

    Create bmi.py

---
+++ bmi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6
+
+# BMI = (weight in kg / height in meters squared)
+# Imperial version: BMI * 703
+
+def gather_info():
+    height = float(input("What is your height? (inches or meters) "))
+    
